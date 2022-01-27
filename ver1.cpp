a0580f708580925ab682ec56312dc1bbecf8162c
davidcerny <david.cerny1@gmail.com>
2022-01-27 17:20:10 -0600

    Eliminated the low pop./high pop. distinction

--- ver1.cpp
+++ ver1.cpp
@@ -74,19 +74,18 @@ int main(int argc, char** argv)
     // const string output_prefix = vm["output_prefix"].as<string>();
     const string output_suffix = vm["output_suffix"].as<string>();
     
-    // targets
-    const double lowerBound = 2.176091; // log_10(150)
-    const double upperBound = 2.176091;
+    // body size optimum (log scale)
+    const double targetLbs = 2.176091; // log_10(150)
 
     // outputing the start parameter data
     ofstream pFile("parameters_" + output_suffix + ".txt");
-    pFile << "popSize\thigh_bd\tlow_bd\tmutCount\tendpointsensitivity" << endl; // 
-    pFile << popSize << "\t" << pow(10,upperBound) << "\t" << pow(10,lowerBound) << "\t" << mutCount << "\t" << endpointsensitivity << endl;
+    pFile << "popSize\tbodySize\tmutCount\tendpointsensitivity" << endl; //
+    pFile << popSize << "\t" << pow(10, targetLbs) << "\t" << mutCount << "\t" << endpointsensitivity << endl;
     pFile.close();
 
     // output stream
     ofstream outputFile("endpoints_" + output_suffix + ".txt");
-    outputFile << "type\tvar_bd\tavg_bd\tavg_fit\tgens_passed" << endl;
+    outputFile << "var_bd\tavg_bd\tavg_fit\tgens_passed" << endl;
 
     // looping whole sim
     for(int i = 0; i < reps; i++) {
@@ -94,67 +93,49 @@ int main(int argc, char** argv)
         cout << " " << endl;
         cout << "simulation rep: " << output_suffix << " ------------------------------------------------------------------------" << endl;
         cout << " " << endl;
-        std::vector<double> mutEffectsHigh(mutCount, 0);
-        std::vector<double> mutEffectsLow(mutCount, 0);
+        std::vector<double> mutEffects(mutCount, 0);
         
         // generation files opening
         
-        std::ostringstream h;
-        std::ostringstream l;
-        h << "high_" << output_suffix << ".txt";
-        l << "low_" << output_suffix << ".txt";
+        std::ostringstream params;
+        params << "params_" << output_suffix << ".txt";
+        string pName = params.str();
+        ofstream genFile(pName.c_str());
+        genFile << "gen\tavg_bd\tvar_bd\n";
 
-        string hName = h.str();
-        string lName = l.str();
-
-        ofstream highFile(hName.c_str());
-        ofstream lowFile(lName.c_str());
-        highFile << "gen\tavg_bd\tvar_bd\n";
-        lowFile << "gen\tavg_bd\tvar_bd\n";
-
-        // end of generation output file data
-
+        // generation files closing
 
         // get a mutation list
         std::vector<double> mutList;
         mutList = getMutList(mutCount, traitStDev);
 
         // get phenotypes for starting population
-        double start_lb = 2;
+        double start_lb = 2; // log_10(100)
         
-        // create populations of popSize
+        // create population of specified size
         Individual squirrel(start_lb);
-        std::vector<Individual> highPop(popSize, squirrel);
-        std::vector<Individual> lowPop(popSize, squirrel);
+        std::vector<Individual> Pop(popSize, squirrel);
 
         // burn-in the populations
 
         
         if (burnLength != 0) {
             for(int i = 0; i < burnLength; i++) {
-                evolvePop(highPop, start_lb, popSize, mutList);
-                evolvePop(lowPop, start_lb, popSize, mutList);
+                evolvePop(Pop, start_lb, popSize, mutList);
 
                 if(i == 0) {
                     cout << "first burnin generation: " << endl;
-                    cout << "   low body_size burnin: " << pow(10, (getAverageLB(lowPop)));
+                    cout << "   Avg body size: " << pow(10, (getAverageLB(Pop)));
                     std::vector<double> fitnessArr(popSize, 0.0);
-                    cout << "   Avg fitness low pop: " << getTotalFitness(lowPop, lowerBound, fitnessArr) / popSize << endl;
-                    cout << "   high body_size burnin: " << pow(10, (getAverageLB(highPop)));
-                    // std::vector<double> fitnessArr(popSize, 0.0);
-                    cout << "   Avg fitness high pop: " << getTotalFitness(highPop, upperBound, fitnessArr) / popSize << endl;
+                    cout << "   Avg fitness: " << getTotalFitness(Pop, targetLbs, fitnessArr) / popSize << endl;
                     cout << " " << endl;
                 }
 
                 if(i == burnLength - 1) {
                     cout << "final burnin generation " << "(" << i + 1 << ")" << ": " << endl;
-                    cout << "   low body size burnin: " << pow(10, (getAverageLB(lowPop)));
+                    cout << "   Avg body size: " << pow(10, (getAverageLB(Pop)));
                     std::vector<double> fitnessArr(popSize, 0.0);
-                    cout << "   Avg fitness low pop: " << getTotalFitness(lowPop, lowerBound, fitnessArr) / popSize << endl;
-                    cout << "   high body size burnin: " << pow(10, (getAverageLB(highPop)));
-                    // std::vector<double> fitnessArr(popSize, 0.0);
-                    cout << "   Avg fitness high pop: " << getTotalFitness(highPop, upperBound, fitnessArr) / popSize << endl;
-                    cout << " " << endl;
+                    cout << "   Avg fitness: " << getTotalFitness(Pop, targetLbs, fitnessArr) / popSize << endl;
                     cout << "starting simulation" << endl;
                 }
             }
@@ -163,78 +144,39 @@ int main(int argc, char** argv)
         // evolve the populations
 
         int count = 0;
-        int lowCount = 0;
-        int highCount = 0;
-        bool lowFinished = false;
-        bool highFinished = false;
-        bool done = false;
+        bool finished = false;
         int burnoutLength = burnLength;
         int outputFrequency = 50; // how often (in terms of generations) to print out data
         time_t start = time(0);
         
         // while the average body size is not at the target
-        while(!done && count < gen_limit) {
+        while(!finished && count < gen_limit) {
             
             // outputing all the generation level data:
-            if(count % outputFrequency == 0) {
-                if(!lowFinished) {
-                    string lowData = getData(lowPop);
-                    lowFile << count << "\t" << lowData;
-                }
-                if(!highFinished) {
-                    string highData = getData(highPop);
-                    highFile << count << "\t" << highData;
-                }
+            if(!finished && count % outputFrequency == 0) {
+                string data = getData(Pop);
+                genFile << count << "\t" << data;
             }
 
-            // checking if low pop has finished evolving
-            if(fabs(pow(10, getAverageLB(lowPop)) - pow(10, lowerBound)) > endpointsensitivity) {
-                evolvePop(lowPop, lowerBound, popSize, mutList);
+            // checking if the population has finished evolving
+            if(fabs(pow(10, getAverageLB(Pop)) - pow(10, targetLbs)) > endpointsensitivity) {
+                evolvePop(Pop, targetLbs, popSize, mutList);
                 if(count % outputFrequency == 0) {
                     cout << "generation: " << count;
-                    cout << "   body size low pop: " << pow(10, (getAverageLB(lowPop))); //编一个新的function，得到正常bodysize的数据 - 搞定
+                    cout << "   Avg body size: " << pow(10, (getAverageLB(Pop))); //编一个新的function，得到正常bodysize的数据 - 搞定
                     std::vector<double> fitnessArr(popSize, 0.0);
-                    cout << "   Avg fitness low pop: " << getTotalFitness(lowPop, lowerBound, fitnessArr) / popSize << endl;
+                    cout << "   Avg fitness: " << getTotalFitness(Pop, targetLbs, fitnessArr) / popSize << endl;
                     
                     // warnings
-                    double lowTraitVal = getAverageLB(lowPop);
-                    if(lowTraitVal < (log(10.1))) {
-                        cout << "   Low pop. approaching lower bound on body size   ";
-                    } else if(lowTraitVal > (log(990.0))) {
-                        cout << "   Low pop. approaching upper bound on body size   ";
+                    double traitVal = getAverageLB(Pop);
+                    if(traitVal < (log(10.1))) {
+                        cout << "   Approaching lower bound on body size   ";
+                    } else if(traitVal > (log(990.0))) {
+                        cout << "   Approaching upper bound on body size   ";
                     }
                 }
-            } else if(!lowFinished) {
-                lowFinished = true;
-                lowCount = count;
-                if(lowFinished && highFinished) {
-                    done = true;
-                }
-            }
-
-            // checking if high pop has finished evolving
-            if(fabs(pow(10, upperBound) - pow(10, getAverageLB(highPop))) > endpointsensitivity) {
-                evolvePop(highPop, upperBound, popSize, mutList);
-                if(count % outputFrequency == 0) {
-                    cout << "generation: " << count;
-                    cout << "   Body Size high pop: " << pow(10, (getAverageLB(highPop)));
-                    std::vector<double> fitnessArr(popSize, 0.0);
-                    cout << "   Avg fitness high pop: " << getTotalFitness(highPop, upperBound, fitnessArr) / popSize << endl;
-                    
-                    // warnings
-                    double highTraitVal = getAverageLB(highPop);
-                    if(highTraitVal < log(10.1)) {
-                        cout << "   High pop. approaching lower bound on body size   ";
-                    } else if(highTraitVal > log(990.0)) {
-                        cout << "   High pop. approaching upper bound on body size   ";
-                    }
-                }
-            } else if(!highFinished) {
-                highCount = count;
-                highFinished = true;
-                if(lowFinished && highFinished) {
-                    done = true;
-                }
+            } else {
+                finished = true;
             }
             
             count++;
@@ -244,8 +186,7 @@ int main(int argc, char** argv)
         cout << "ending simulation" << endl;
         cout << " " << endl;
         cout << "final stats: " << endl;
-        cout << "high pop body size: " << pow(10, (getAverageLB(highPop))) << " " << "   high pop generations: " << highCount << endl;
-        cout << "low pop body size: " << pow(10, (getAverageLB(lowPop))) << " " << "   low pop generations: " << lowCount << endl;
+        cout << "avg body size: " << pow(10, (getAverageLB(Pop))) << " " << "   generations: " << count << endl;
         
         // print total time
         double diff = difftime(time(0), start);
@@ -253,12 +194,10 @@ int main(int argc, char** argv)
 
         // print endpoint data
         std::vector<double> fitnessArr(popSize, 0.0);
-        outputFile << "low\t" << getVmor(lowPop) << "\t" << pow(10, (getAverageLB(lowPop)))<< "\t" << getTotalFitness(lowPop, lowerBound, fitnessArr) / popSize << "\t" << lowCount << endl;
-        outputFile << "high\t" << getVmor(highPop) << "\t" << pow(10, (getAverageLB(highPop))) << "\t" << getTotalFitness(highPop, upperBound, fitnessArr) / popSize << "\t" << highCount << endl;
+        outputFile << getVmor(Pop) << "\t" << pow(10, (getAverageLB(Pop)))<< "\t" << getTotalFitness(Pop, targetLbs, fitnessArr) / popSize << "\t" << count << endl;
 
         // closing output generation files
-        highFile.close();
-        lowFile.close();
+        genFile.close();
         
     }
     return 0;
